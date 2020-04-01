c0cb9d434b3dbd835466ae2d3aacc3bcafb65da3
Alina Andreeva <andreeva.a.wezom@gmail.com>
2020-04-01 17:05:49 +0300

    static profile info

--- app/src/components/Profile/Sidebar/Sidebar.tsx
+++ app/src/components/Profile/Sidebar/Sidebar.tsx
@@ -1,9 +1,26 @@
-import React, {FC, useState} from 'react';
+import React, {FC, useState, useEffect, Fragment} from 'react';
 import {ProfileType} from "../../../redux/reducers/types";
 import withSidebarStyles from './sidebarStyles'
-import {WithStyles, Paper, Avatar, Typography} from '@material-ui/core';
+import {
+    WithStyles,
+    Paper,
+    Avatar,
+    Typography,
+    List,
+    ListItem,
+    Divider,
+    Link
+} from '@material-ui/core';
 import userPlaceholder from "../../../assets/images/user-placeholder.png";
 import ProfileStatus from "../Status/ProfileStatus";
+import FacebookIcon from '@material-ui/icons/Facebook';
+import LanguageIcon from '@material-ui/icons/Language';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import VkIcon from '../../Svg/VkIcon';
+import classNames from "classnames";
 
 type PropsType = {
     status: string,
@@ -12,18 +29,66 @@ type PropsType = {
     isOwner: boolean
 }
 
-const Sidebar: FC<PropsType & WithStyles> = ({profile: {photos, fullName}, status, classes, setUserStatus, isOwner}) => {
-    const userAvatar = photos.small !== null ? photos.small : userPlaceholder;
+type IconsType = {
+    [key: string]: any
+};
+
+const icons: IconsType = {
+    facebook: FacebookIcon,
+    website: LanguageIcon,
+    vk: VkIcon,
+    twitter: TwitterIcon,
+    instagram: InstagramIcon,
+    youtube: YouTubeIcon,
+    github: GitHubIcon,
+};
+
+type ContactsArrType = {
+    name: string
+    url: string
+}
+
+const SocialIcon = (props: any) => {
+    const {name, ...other} = props;
+    const TagName = icons[name];
+    return <TagName {...other}/>
+};
+
+const Sidebar: FC<PropsType & WithStyles> = ({profile: {photos, fullName, contacts}, status, classes, setUserStatus, isOwner}) => {
+    const userAvatar = photos.large !== null ? photos.large : userPlaceholder;
+    const [contactsArr, setContactsArr] = useState<Array<ContactsArrType>>([]);
+    useEffect(() => {
+        let newArr: ContactsArrType[] = [];
+        Object.entries(contacts).forEach(([key, value]) => {
+                if (value !== null && value.length) {
+                    newArr = [...newArr, {name: key, url: value}];
+                }
+            }
+        );
+        setContactsArr(newArr);
+    }, [contacts]);
 
     return (
         <Paper className={classes.paper}>
             <div className={classes.body}>
-                <div className={classes.avatar}>
-                    <Avatar src={userAvatar} alt={fullName ? fullName : 'avatar'} className={classes.avatarImg}/>
-                </div>
+                <Avatar src={userAvatar} alt={fullName ? fullName : 'avatar'} className={classes.avatarImg}/>
                 <Typography variant="h6">{fullName}</Typography>
-               <ProfileStatus status={status} setUserStatus={setUserStatus} isOwner={isOwner}/>
+                <ProfileStatus status={status} setUserStatus={setUserStatus} isOwner={isOwner}/>
             </div>
+            <List aria-label="socials" disablePadding={true} className={classes.socials} component="div">
+                    {
+                        contactsArr.map(({name, url}: {name: any, url: string}) =>
+                            <Fragment key={name}>
+                                <Divider/>
+                                <ListItem button component={Link} href={url} target="_blank" rel="noopener noreferrer">
+                                    <SocialIcon name={name} className={classNames(`${classes.socialsIcon}`, `${name}`)}/>
+                                    <Typography variant="body2" noWrap>{url}</Typography>
+                                </ListItem>
+                            </Fragment>
+                        )
+                    }
+
+            </List>
         </Paper>
     )
 };
--- app/src/components/Profile/Sidebar/sidebarStyles.ts
+++ app/src/components/Profile/Sidebar/sidebarStyles.ts
@@ -1,25 +1,48 @@
 import {withStyles, createStyles, Theme} from '@material-ui/core/styles';
+import {} from '@material-ui/core/colors';
 
 const styles = createStyles((theme: Theme) => ({
     paper: {
         borderRadius: 0,
-        paddingBottom: 30,
         backgroundColor: theme.palette.common.white
     },
     body: {
-        textAlign: 'center'
-    },
-    avatar: {
-
+        textAlign: 'center',
+        paddingBottom: 20,
     },
     avatarImg: {
-        marginBottom: '-100px',
+        marginBottom: '-90px',
         height: 200,
         width: 200,
         border: '3px solid #fff',
         margin: '0 auto',
         position: 'relative',
         top:' -100px'
+    },
+    socialsIcon: {
+        height: 25,
+        width: 25,
+        marginRight: 20,
+
+        '&.facebook': {
+            color: '#3b5999'
+        },
+
+        '&.twitter': {
+            color: '#55acee'
+        },
+
+        '&.instagram': {
+            color: '#e4405f'
+        },
+
+        '&.youtube': {
+            color: '#cd201f'
+        },
+
+        '&.github': {
+            color: '#000'
+        }
     },
 }));
 
